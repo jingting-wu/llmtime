287e72387b923b1ce752259c8513735733625b9b
jingting-wu <jingting.wu@coolblue.nl>
2024-07-10 14:11:45 +0200

    only use gpt

--- models/llms.py
+++ models/llms.py
@@ -1,14 +1,14 @@
 from functools import partial
 from models.gpt import gpt_completion_fn, gpt_nll_fn
 from models.gpt import tokenize_fn as gpt_tokenize_fn
-from models.llama import llama_completion_fn, llama_nll_fn
-from models.llama import tokenize_fn as llama_tokenize_fn
-
-from models.mistral import mistral_completion_fn, mistral_nll_fn
-from models.mistral import tokenize_fn as mistral_tokenize_fn
-
-from models.mistral_api import mistral_api_completion_fn, mistral_api_nll_fn
-from models.mistral_api import tokenize_fn as mistral_api_tokenize_fn
+# from models.llama import llama_completion_fn, llama_nll_fn
+# from models.llama import tokenize_fn as llama_tokenize_fn
+#
+# from models.mistral import mistral_completion_fn, mistral_nll_fn
+# from models.mistral import tokenize_fn as mistral_tokenize_fn
+#
+# from models.mistral_api import mistral_api_completion_fn, mistral_api_nll_fn
+# from models.mistral_api import tokenize_fn as mistral_api_tokenize_fn
 
 
 # Required: Text completion function for each model
@@ -30,16 +30,16 @@ completion_fns = {
     'gpt-4': partial(gpt_completion_fn, model='gpt-4'),
     'gpt-4-1106-preview':partial(gpt_completion_fn, model='gpt-4-1106-preview'),
     'gpt-3.5-turbo-instruct': partial(gpt_completion_fn, model='gpt-3.5-turbo-instruct'),
-    'mistral': partial(mistral_completion_fn, model='mistral'),
-    'mistral-api-tiny': partial(mistral_api_completion_fn, model='mistral-tiny'),
-    'mistral-api-small': partial(mistral_api_completion_fn, model='mistral-small'),
-    'mistral-api-medium': partial(mistral_api_completion_fn, model='mistral-medium'),
-    'llama-7b': partial(llama_completion_fn, model='7b'),
-    'llama-13b': partial(llama_completion_fn, model='13b'),
-    'llama-70b': partial(llama_completion_fn, model='70b'),
-    'llama-7b-chat': partial(llama_completion_fn, model='7b-chat'),
-    'llama-13b-chat': partial(llama_completion_fn, model='13b-chat'),
-    'llama-70b-chat': partial(llama_completion_fn, model='70b-chat'),
+    # 'mistral': partial(mistral_completion_fn, model='mistral'),
+    # 'mistral-api-tiny': partial(mistral_api_completion_fn, model='mistral-tiny'),
+    # 'mistral-api-small': partial(mistral_api_completion_fn, model='mistral-small'),
+    # 'mistral-api-medium': partial(mistral_api_completion_fn, model='mistral-medium'),
+    # 'llama-7b': partial(llama_completion_fn, model='7b'),
+    # 'llama-13b': partial(llama_completion_fn, model='13b'),
+    # 'llama-70b': partial(llama_completion_fn, model='70b'),
+    # 'llama-7b-chat': partial(llama_completion_fn, model='7b-chat'),
+    # 'llama-13b-chat': partial(llama_completion_fn, model='13b-chat'),
+    # 'llama-70b-chat': partial(llama_completion_fn, model='70b-chat'),
 }
 
 # Optional: NLL/D functions for each model
@@ -61,17 +61,17 @@ completion_fns = {
 #   - float: Computed NLL per dimension for p(target_arr | input_arr).
 nll_fns = {
     'text-davinci-003': partial(gpt_nll_fn, model='text-davinci-003'),
-    'mistral': partial(mistral_nll_fn, model='mistral'),
-    'mistral-api-tiny': partial(mistral_api_nll_fn, model='mistral-tiny'),
-    'mistral-api-small': partial(mistral_api_nll_fn, model='mistral-small'),
-    'mistral-api-medium': partial(mistral_api_nll_fn, model='mistral-medium'),
-    'llama-7b': partial(llama_completion_fn, model='7b'),
-    'llama-7b': partial(llama_nll_fn, model='7b'),
-    'llama-13b': partial(llama_nll_fn, model='13b'),
-    'llama-70b': partial(llama_nll_fn, model='70b'),
-    'llama-7b-chat': partial(llama_nll_fn, model='7b-chat'),
-    'llama-13b-chat': partial(llama_nll_fn, model='13b-chat'),
-    'llama-70b-chat': partial(llama_nll_fn, model='70b-chat'),
+    # 'mistral': partial(mistral_nll_fn, model='mistral'),
+    # 'mistral-api-tiny': partial(mistral_api_nll_fn, model='mistral-tiny'),
+    # 'mistral-api-small': partial(mistral_api_nll_fn, model='mistral-small'),
+    # 'mistral-api-medium': partial(mistral_api_nll_fn, model='mistral-medium'),
+    # 'llama-7b': partial(llama_completion_fn, model='7b'),
+    # 'llama-7b': partial(llama_nll_fn, model='7b'),
+    # 'llama-13b': partial(llama_nll_fn, model='13b'),
+    # 'llama-70b': partial(llama_nll_fn, model='70b'),
+    # 'llama-7b-chat': partial(llama_nll_fn, model='7b-chat'),
+    # 'llama-13b-chat': partial(llama_nll_fn, model='13b-chat'),
+    # 'llama-70b-chat': partial(llama_nll_fn, model='70b-chat'),
 }
 
 # Optional: Tokenization function for each model, only needed if you want automatic input truncation.
@@ -84,16 +84,16 @@ nll_fns = {
 tokenization_fns = {
     'text-davinci-003': partial(gpt_tokenize_fn, model='text-davinci-003'),
     'gpt-3.5-turbo-instruct': partial(gpt_tokenize_fn, model='gpt-3.5-turbo-instruct'),
-    'mistral': partial(mistral_tokenize_fn, model='mistral'),
-    'mistral-api-tiny': partial(mistral_api_tokenize_fn, model='mistral-tiny'),
-    'mistral-api-small': partial(mistral_api_tokenize_fn, model='mistral-small'),
-    'mistral-api-medium': partial(mistral_api_tokenize_fn, model='mistral-medium'),
-    'llama-7b': partial(llama_tokenize_fn, model='7b'),
-    'llama-13b': partial(llama_tokenize_fn, model='13b'),
-    'llama-70b': partial(llama_tokenize_fn, model='70b'),
-    'llama-7b-chat': partial(llama_tokenize_fn, model='7b-chat'),
-    'llama-13b-chat': partial(llama_tokenize_fn, model='13b-chat'),
-    'llama-70b-chat': partial(llama_tokenize_fn, model='70b-chat'),
+    # 'mistral': partial(mistral_tokenize_fn, model='mistral'),
+    # 'mistral-api-tiny': partial(mistral_api_tokenize_fn, model='mistral-tiny'),
+    # 'mistral-api-small': partial(mistral_api_tokenize_fn, model='mistral-small'),
+    # 'mistral-api-medium': partial(mistral_api_tokenize_fn, model='mistral-medium'),
+    # 'llama-7b': partial(llama_tokenize_fn, model='7b'),
+    # 'llama-13b': partial(llama_tokenize_fn, model='13b'),
+    # 'llama-70b': partial(llama_tokenize_fn, model='70b'),
+    # 'llama-7b-chat': partial(llama_tokenize_fn, model='7b-chat'),
+    # 'llama-13b-chat': partial(llama_tokenize_fn, model='13b-chat'),
+    # 'llama-70b-chat': partial(llama_tokenize_fn, model='70b-chat'),
 }
 
 # Optional: Context lengths for each model, only needed if you want automatic input truncation.
